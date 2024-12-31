500b9b3b98a33f8755f921613a8f2e1ecae959df
Fred Nix <frednix@gmail.com>
2024-12-31 16:13:03 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
-  description = "NixOS Configuration for nixosvm with Flakes";
+  description = "NixOS Configuration for nixos with Flakes and Home Manager";
 
   # Inputs
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.home-manager.url = "github:nix-community/home-manager";
 
   # Outputs
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixosvm = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
       ];
     };
   };
